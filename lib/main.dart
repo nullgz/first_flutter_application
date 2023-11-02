03e3977ed9163f93d6c9dd4caf53e2afe3a74a0e
nullgz <nullnull.gz@gmail.com>
2023-11-01 21:50:23 -0700

    Studied about stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,50 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Ng\'s Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Second Day of Flutter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimalName('Dog'),
+              SizedBox(height: 8),
+              AnimalName('Cat'),
+              SizedBox(height: 8),
+              AnimalName('Bird'),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'First Flutter App'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class AnimalName extends StatelessWidget {
+  final String name;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const AnimalName(this.name);
 
   @override
-  Widget build(BuildContext context) {    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        // backgroundColor: Theme.of(context).colorScheme.tertiary,        
-        title: Text(widget.title),
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.lightBlue),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(name),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
