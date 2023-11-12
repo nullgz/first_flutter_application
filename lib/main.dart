522fddb6fd985672d3df3602a585e844f9707cef
nullgz <nullnull.gz@gmail.com>
2023-11-12 13:37:34 -0800

    #2 play with statelee & stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,104 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ng\'s Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Second Day of Flutter'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimalName('Dog'),
-              SizedBox(height: 8),
-              AnimalName('Cat'),
-              SizedBox(height: 8),
-              AnimalName('Bird'),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AnimalName extends StatelessWidget {
+class Product {
   final String name;
+  const Product({required this.name});
+}
 
-  const AnimalName(this.name);
+typedef CartChangedCallback = Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  ShoppingListItem({
+    required this.product,
+    required this.inCart,
+    required this.onCartChanged,
+  }) : super(key: ObjectKey(product));
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black45 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return const TextStyle(
+        color: Colors.black54, decoration: TextDecoration.lineThrough);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.lightBlue),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(name),
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(
+        product.name,
+        style: _getTextStyle(context),
       ),
     );
   }
+}
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, super.key});
+
+  final List<Product> products;
+
+  @override
+  State<StatefulWidget> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ng 의 쇼핑 목록'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: widget.products.map((product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Shopping app',
+    home: ShoppingList(
+      products: [
+        Product(name: 'eggs'),
+        Product(name: 'flour'),
+        Product(name: 'chocolate chips'),
+      ],
+    ),
+  ));
 }
